df43ebaf5904e29bbf16d3bb4204b31cf48d3d6d
pepelu8000 <joselu_1993@hotmail.es>
2016-09-30 11:28:34 +0200

    User interface: sidebar panel finished

--- ui.R
+++ ui.R
@@ -10,7 +10,28 @@ shinyUI(fluidPage(
                    c("Address" = "ADDRESS",
                      "Latitude and Longitude" = "LAT_LONG",
                      "CSV file" = "CSV")),
-      
+      conditionalPanel(
+        condition = "input.input_type == 'ADDRESS'",
+        textInput("address_t", "Type an address:", placeholder = "Skattegarden 29B, Linköping")
+      ),
+      conditionalPanel(
+        condition = "input.input_type == 'LAT_LONG'",
+        textInput("lat_t", "Type a latitude value:", placeholder = "58.4290005"),
+        textInput("long_t", "Type a longitude value:", placeholder = "15.5799367")
+      ),
+      conditionalPanel(
+        condition = "input.input_type == 'CSV'",
+        fileInput('csv_file', 'Choose a file to upload',
+                  accept = c(
+                    'text/csv',
+                    'text/comma-separated-values',
+                    'text/tab-separated-values',
+                    'text/plain',
+                    '.csv',
+                    '.tsv'
+                  )
+        )
+      )
     ),
     
     # Show map
