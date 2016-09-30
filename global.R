373284c67012bf4c2b03eeadab6bed61fc30df76
pepelu8000 <joselu_1993@hotmail.es>
2016-09-30 11:27:10 +0200

    User interface: first draft

--- global.R
+++ global.R
@@ -1,1 +1,4 @@
 library(shiny)
+library(devtools)
+devtools::install_github("tenoglez/ARP_Laboratory5", subdir="geoloc", quiet = TRUE)
+library(geoloc)
--- ui.R
+++ ui.R
@@ -1,2 +1,23 @@
-shinyUI(fluidPage())
+shinyUI(fluidPage(  
   
+  headerPanel("Shiny app using package geoloc"),
+  
+  sidebarLayout(
+    # Sidebar 
+    sidebarPanel(
+      # radio
+      radioButtons("input_type", "Select an input:",
+                   c("Address" = "ADDRESS",
+                     "Latitude and Longitude" = "LAT_LONG",
+                     "CSV file" = "CSV")),
+      
+    ),
+    
+    # Show map
+    mainPanel()
+    
+  )
+  
+  
+  
+))
