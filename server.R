f207d837a154a25221779202fc4ab7c1e80a72b8
tenoglez <teno_glez@hotmail.com>
2016-09-30 11:35:51 +0200

    Server file: address input case finished

--- server.R
+++ server.R
@@ -4,6 +4,10 @@ shinyServer(function(input, output) {
   output$mymap <- renderLeaflet({
     # empty dataframe
     address_result <- data.frame(latitude = numeric(0), longitude = numeric(0), address = character(0))
+    # initial view parameters (whole map centered)
+    center.lat <- 30
+    center.long <- 11
+    init.zoom <- 2
     
     # input is a CSV and the file is working
     if ((input$input_type == "CSV") && (!is.null(input$csv_file))) {
@@ -12,15 +16,22 @@ shinyServer(function(input, output) {
       # input is an address
     }else if (input$input_type == "ADDRESS") {
       
-      
+      address_result <- geo_connect_vector(input$address_t)
+      # specific view parameters only if the user's input is correct
+      if ((input$address_t != "") && (address_result$address != "ERROR")) {
+        center.long <- address_result$longitude
+        center.lat <- address_result$latitude
+        init.zoom <- 15
+      }
       # coordinates as input
     } else if (input$input_type == "LAT_LONG") {
-      
+
       
     }
     
+    address_result <- subset(address_result, address_result$address != "ERROR")
     return(leaflet(data = address_result) %>%
-             setView(lat = 30, lng = 11, zoom = 2) %>%
+             setView(lat = center.lat, lng = center.long, zoom = init.zoom) %>%
              addTiles() %>%
              addMarkers(lng = ~longitude, ~latitude, popup = ~as.character(address)))
   })
